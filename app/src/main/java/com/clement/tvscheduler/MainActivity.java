51c2375be520b069c5ac83253b8c7d21020a79b7
clementsoullard <clementsoullard@yahoo.fr>
2016-09-01 10:20:40 +0100

    Ajout de la boite de dialogue

--- app/src/main/java/com/clement/tvscheduler/MainActivity.java
+++ app/src/main/java/com/clement/tvscheduler/MainActivity.java
@@ -1,35 +1,46 @@
 package com.clement.tvscheduler;
 
+import android.app.Activity;
+import android.app.DialogFragment;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.clement.tvscheduler.dialog.PinDialog;
 import com.clement.tvscheduler.task.CreditTask;
 import com.clement.tvscheduler.task.PunitionTask;
 import com.clement.tvscheduler.task.TVStatusTask;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     public final static String TAG = "MainActivity";
 
     private Button tvOn;
+
     private Button tvOff;
+
     private Button tvCredit30;
+
     private Button punition;
+
     private Button prive;
+
     private Button recompense;
+
     private Button tvCredit60;
+
     private TextView remainingTimeView;
+
     private TextView relayStatusView;
 
     @Override
@@ -47,85 +58,127 @@ public class MainActivity extends AppCompatActivity {
         tvOff = (Button) findViewById(R.id.button_off);
         tvCredit30 = (Button) findViewById(R.id.button_30);
         tvCredit60 = (Button) findViewById(R.id.button_60);
-        punition=(Button) findViewById(R.id.button_punition);
-        prive=(Button) findViewById(R.id.button_prive);
-        recompense=(Button) findViewById(R.id.button_recompense);
+        punition = (Button) findViewById(R.id.button_punition);
+        prive = (Button) findViewById(R.id.button_prive);
+        recompense = (Button) findViewById(R.id.button_recompense);
         remainingTimeView = (TextView) findViewById(R.id.remainingTime_view);
         relayStatusView = (TextView) findViewById(R.id.relayStatus_view);
 
         tvOn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CreditTask creditTask = new CreditTask(MainActivity.this);
                 Log.i(TAG, "Click sur TV ON");
-                creditTask.execute(-2);
+                enterPin();
+                requestServerCredit(-2);
+
             }
         });
         tvOff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CreditTask creditTask = new CreditTask(MainActivity.this);
                 Log.i(TAG, "Click sur TV Off");
-                creditTask.execute(-1);
+                requestServerCredit(-1);
             }
         });
         tvCredit30.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CreditTask creditTask = new CreditTask(MainActivity.this);
                 Log.i(TAG, "Click sur TV 30");
-                creditTask.execute(1800);
+                requestServerCredit(1800);
             }
         });
         tvCredit60.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CreditTask creditTask = new CreditTask(MainActivity.this);
                 Log.i(TAG, "Click sur TV 60");
-                creditTask.execute(3600);
+                requestServerCredit(3600);
             }
         });
         punition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PunitionTask puntionTask = new PunitionTask(MainActivity.this);
                 Log.i(TAG, "Click sur punition");
-                puntionTask.execute(-20);
+                requestServerPunition(-20);
             }
         });
         prive.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PunitionTask puntionTask = new PunitionTask(MainActivity.this);
                 Log.i(TAG, "Click sur punition");
-                puntionTask.execute(-1000);
+                requestServerPunition(-1000);
+
             }
         });
 
         recompense.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PunitionTask puntionTask = new PunitionTask(MainActivity.this);
                 Log.i(TAG, "Click sur punition");
-                puntionTask.execute(20);
+                requestServerPunition(20);
             }
         });
 
-        TVStatusTask tvStatusTask=new TVStatusTask(this);
+        TVStatusTask tvStatusTask = new TVStatusTask(this);
         tvStatusTask.execute();
     }
 
     public void credited(String message) {
         Context context = getApplicationContext();
-        CharSequence text =message;
+        CharSequence text = message;
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
     }
+
     public void setTimeRemaining(String timeRemaining) {
         remainingTimeView.setText(timeRemaining);
     }
+
     public void setRelayStatus(String relayStatus) {
         relayStatusView.setText(relayStatus);
+    }
+
+    /**
+     * Reauest a credit to the server
+     *
+     * @param credit
+     */
+    void requestServerCredit(int credit) {
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (mWifi.isConnected()) {
+            Log.i(TAG, "Network is ok");
+            CreditTask creditTask = new CreditTask(MainActivity.this);
+            creditTask.execute(credit);
+        } else {
+            Log.i(TAG, "There is no network doing nothing");
+        }
+    }
+
+    /**
+     * Reauest a credit to the server
+     *
+     * @param punition
+     */
+    void requestServerPunition(int punition) {
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (mWifi.isConnected()) {
+            Log.i(TAG, "Network is ok");
+            PunitionTask puntionTask = new PunitionTask(MainActivity.this);
+            puntionTask.execute(punition);
+        } else {
+            Log.i(TAG, "There is no network doing nothing");
+        }
+
+    }
+
+    /**
+     * This creates a dialog to enter the pin
+     */
+    public void enterPin() {
+        android.support.v4.app.DialogFragment newFragment = new PinDialog();
+        FragmentManager fm=getSupportFragmentManager();
+        newFragment.show(fm,"pin");
     }
 }
